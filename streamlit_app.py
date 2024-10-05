b215e44719035307955d9967302ed0980cf3bc49
JDrost04 <533567@student.fontys.nl>
2024-10-05 15:03:16 +0200

    sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
-st.logo("Logo_transdev.png", size='large')
-
 st.logo("Logo_transdev_klein.png", size='large', icon_image="Logo_transdev_groot.png")
-st.sidebar.markdown("Hi!")
+
+# Add a title to the sidebar
+st.sidebar.title("Navigation")
+
+# Add links to the sidebar
+st.sidebar.markdown("[Bus Planning Checker]")
+st.sidebar.markdown("[How it Works]")
+st.sidebar.markdown("[Help]")st.sidebar.markdown("Hi!")
 
 st.title("🚌 Bus Planning Checker")
 st.write(
