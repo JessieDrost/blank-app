6a2d65b54b3a2277699c6bc438cedac92dc4972d
JDrost04 <533567@student.fontys.nl>
2024-10-05 16:04:30 +0200

    hsdbv

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title("Bus Planning Checker")
 st.write(
     "Instantly validate your circulation planning for compliance!"
 )
-bus_checker = st.Page("bus_check.py", title="Bus Planning Checker", icon=":material/search:")
+bus_checker = st.Page("bus_check.py", title="Bus Planning Checker")
 how_work = st.Page("how_it_works.py", title="How It Works")
 help = st.Page("help.py", title="How To Use")
 
