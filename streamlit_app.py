070a44c40609ec69ef7dc66296bd84b641c18297
JDrost04 <533567@student.fontys.nl>
2024-10-05 15:15:36 +0200

    multipage

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,13 @@ st.logo("Logo_transdev_klein.png", size='large', icon_image="tra_logo_rgb_LR.jpg
 st.sidebar.title("Navigation")
 
 # Add links to the sidebar
-st.sidebar.markdown("[Bus Planning Checker]")
-st.sidebar.markdown("[How it Works]")
-st.sidebar.markdown("[Help]")
+st.sidebar.markdown("Bus Planning Checker")
+st.sidebar.markdown("How it Works")
+st.sidebar.markdown("Help")
 
-st.title("🚌 Bus Planning Checker")
+st.title("Bus Planning Checker")
 st.write(
     "Instantly validate your circulation planning for compliance!"
 )
+pg = st.navigation([st.Page("bus_planning_checker.py"), st.Page("How_it_works.py"), st.Page("help.py")])
+pg.run()
