fa4f76c6068f535c35557a6c59a514de04be62fe
JDrost04 <533567@student.fontys.nl>
2024-10-05 16:15:39 +0200

    log

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Display the logo
-st.logo("Logo_transdev_klein.png", size="large", icon_image="tra_logo_CMYK_HR.jpg")
+st.logo("tra_logo_CMYK_HR.jpg", size="large", icon_image="Logo_transdev_klein.png")
 
 # Define pages
 bus_checker = st.Page("bus_check.py", title="Bus Planning Checker")
