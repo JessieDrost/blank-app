1b7eeae13f679ef494782f90bd7bba883423933a
JDrost04 <533567@student.fontys.nl>
2024-10-05 14:02:38 +0200

    colors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
 st.logo("Logo_transdev.png")
+
+st.primaryColor = "#0000FF"
+st.backgroundColor = "#FFFFFF"
+st.secondaryBackgroundColor = "#F0F0F0"
+st.textColor = "#000000"
+
 st.title("🚌 Bus Planning Checker")
 st.write(
     "Instantly validate your circulation planning for compliance!"
