9148f9ce78da1e0b935d8fdd6138109d165d2649
JDrost04 <533567@student.fontys.nl>
2024-10-05 15:19:22 +0200

    nav

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit as st
 
 st.logo("Logo_transdev_klein.png", size='large', icon_image="tra_logo_rgb_LR.jpg")
 
-# Add a title to the sidebar
-st.sidebar.title("Navigation")
-
 # Add links to the sidebar
 pg = st.navigation([st.Page("bus_planning_checker.py"), st.Page("How_it_works.py"), st.Page("help.py")])
 pg.run()
