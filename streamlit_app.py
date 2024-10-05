0b356f0f96edd971da854e2b9012e3d54f24b07c
JDrost04 <533567@student.fontys.nl>
2024-10-05 16:09:29 +0200

    multipage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 
+# Display the logo
 st.logo("Logo_transdev_klein.png", size="large", icon_image="tra_logo_rgb_LR.jpg")
 
-
+# Title and description
 st.title("Bus Planning Checker")
-st.write(
-    "Instantly validate your circulation planning for compliance!"
-)
+st.write("Instantly validate your circulation planning for compliance!")
+
+# Define pages
 bus_checker = st.Page("bus_check.py", title="Bus Planning Checker")
 how_work = st.Page("how_it_works.py", title="How It Works")
-help = st.Page("help.py", title="How To Use")
+help_page = st.Page("help.py", title="Help")
 
-pg = st.navigation(bus_checker, how_work, help)
+# Define the navigation menu with the pages
+pg = st.navigation([bus_checker, how_work, help_page])
+
+# Run the selected page
 pg.run()
