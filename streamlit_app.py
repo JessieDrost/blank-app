f04c2a6b9085ba580ae0d80906199d7c23b91625
JDrost04 <533567@student.fontys.nl>
2024-10-05 14:58:17 +0200

    logo sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.logo("Logo_transdev.png", size='large')
 
-st.sidebar.write('How it works')
-st.sidebar.button('click here')
+st.logo("Logo_transdev_klein.png", size='large', icon_image="Logo_transdev_groot.png")
+st.sidebar.markdown("Hi!")
 
 st.title("🚌 Bus Planning Checker")
 st.write(
