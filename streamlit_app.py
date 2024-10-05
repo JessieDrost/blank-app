e36ad0c19846fda258a27316e8ae56129cb745f8
JDrost04 <533567@student.fontys.nl>
2024-10-05 14:46:48 +0200

    sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.logo("Logo_transdev.png", size='large')
 
 st.sidebar.write('How it works')
-
+st.sidebar.button('click here')
 
 st.title("🚌 Bus Planning Checker")
 st.write(
