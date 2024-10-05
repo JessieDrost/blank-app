7a1b91787c7077a6ac9b3505dbcd622c2a395b50
JDrost04 <533567@student.fontys.nl>
2024-10-05 16:23:38 +0200

    bhds

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import streamlit as st
 st.logo("Logo_transdev_klein.png", size="large")
 
 # Define pages
-bus_checker = st.Page("bus_check.py", title=":bus: Bus Planning Checker")
-how_work = st.Page("how_it_works.py", title=":book: How It Works")
-help_page = st.Page("help.py", title=":question: Help")
+bus_checker = st.Page("bus_check.py", title="🚌 Bus Planning Checker")
+how_work = st.Page("how_it_works.py", title="📖 How It Works")
+help_page = st.Page("help.py", title="❓Help")
 
 # Define the navigation menu with the pages
 pg = st.navigation([bus_checker, how_work, help_page])
