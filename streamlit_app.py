071558a3dae3ce3bf29a2366b6ec8460212b6998
JDrost04 <533567@student.fontys.nl>
2024-10-05 15:20:07 +0200

    capitalization

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.logo("Logo_transdev_klein.png", size='large', icon_image="tra_logo_rgb_LR.jpg")
 
 # Add links to the sidebar
-pg = st.navigation([st.Page("bus_planning_checker.py"), st.Page("How_it_works.py"), st.Page("help.py")])
+pg = st.navigation([st.Page("Bus_Planning_Checker.py"), st.Page("How_it_Works.py"), st.Page("Help.py")])
 pg.run()
 
 st.title("Bus Planning Checker")
