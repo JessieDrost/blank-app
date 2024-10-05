07d1fe9450020c6a15e0257f93f3b6c74e6def2a
JDrost04 <533567@student.fontys.nl>
2024-10-05 15:51:44 +0200

    image

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,16 @@ import streamlit as st
 
 st.logo("Logo_transdev_klein.png", size='large', icon_image="tra_logo_rgb_LR.jpg")
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("");
-background-size: cover;
-}https://brandguide.transdev.nl/files/220504-transdev-vdl-citea-huizen-0022-bewerkt-1.jpg
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://brandguide.transdev.nl/files/220504-transdev-vdl-citea-huizen-0022-bewerkt-1.jpg")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Add a title to the sidebar
 st.sidebar.title("Navigation")
