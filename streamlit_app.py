695392dba60a05f9706b39b7581ddecd8e57530b
JDrost04 <533567@student.fontys.nl>
2024-10-05 15:43:34 +0200

    bi

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,15 @@ import streamlit as st
 
 st.logo("Logo_transdev_klein.png", size='large', icon_image="tra_logo_rgb_LR.jpg")
 
+# Add a title to the sidebar
+st.sidebar.title("Navigation")
+
 # Add links to the sidebar
-bus_planning_checker = st.Page(
-    "bus_check.py", title="Bus Planning Checker", icon=":bus:")
+st.sidebar.markdown("Bus Planning Checker")
+st.sidebar.markdown("How it Works")
+st.sidebar.markdown("Help")
 
-how_it_works = st.Page(
-    "how_it_works.py", title="How it Works", icon=":book:")
-
-help_page = st.Page(
-    "help.py", title="How To Use", icon=":question:")
-pg = st.navigation([st.Page("Bus_Planning_Checker.py"), st.Page("How_it_Works.py"), st.Page("Help.py")])
-pg.run()
-
-
+st.title("Bus Planning Checker")
+st.write(
+    "Instantly validate your circulation planning for compliance!"
+)
