3ddcc22a044e1ebb67dad995b8314d31f176ab3c
JDrost04 <533567@student.fontys.nl>
2024-10-05 16:11:38 +0200

    redun

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import streamlit as st
 # Display the logo
 st.logo("Logo_transdev_klein.png", size="large", icon_image="tra_logo_rgb_LR.jpg")
 
-# Title and description
-st.title("Bus Planning Checker")
-st.write("Instantly validate your circulation planning for compliance!")
-
 # Define pages
 bus_checker = st.Page("bus_check.py", title="Bus Planning Checker")
 how_work = st.Page("how_it_works.py", title="How It Works")
